b44a56582d365dadbf121d8a2e9b1f3de3053af2
Lorthioir <jeremy.lorthioir24@gmail.com>
2022-03-11 19:02:44 +0100

    [Improve] Better about page + add resume

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
     // Ce dossier est cloné lors du build
     eleventyConfig.addPassthroughCopy("./src/assets/img");
     eleventyConfig.addPassthroughCopy("./src/assets/css");
+    eleventyConfig.addPassthroughCopy("./src/assets/files");
     eleventyConfig.addPassthroughCopy("./uploads");
 
     return {
